539193470f01311731899fe61778d0f487ea8eb7
fugitiveinkc <blacksg1909@gmail.com>
2016-04-10 00:42:18 -0500

    Function that creates an array of rgb values.  Intended to be a 2D board with colors.

---
+++ board.py
@@ -0,0 +1,18 @@
+'''
+
+Objective: Create a function that generates a board to use flood recursion on.
+
+Libraries: pygame, numpy
+
+Notes: Need to learn how to utilize 3D arrays
+
+'''
+import numpy as np
+
+#Primitive board.  Will update to include possible walls.
+
+def board(x_dim, y_dim, color): #board accepts int, int and (r, g, b)
+	new_board = np.zeros((x_dim, y_dim, 3), dtype=np.int)
+	new_board[:] = color
+	return new_board
+
