23cbf53df5fb6417368b87a7d492e14b7e03c985
Tim Clark <tim.clark@propylon.com>
2016-04-13 13:34:00 -0500

    Add stack-based flood select, remove non-numpy-array test

--- flood.py
+++ flood.py
@@ -1,4 +1,6 @@
-#Should we make this iterative or just specify a larger block to check for each recursive call?
+"""Perform flood algorithms on numpy arrays."""
+import numpy
+
 
 def flood_fill(array, start_pos, fill_value):
     """Fill contiguous region of an array with a new value"""
@@ -6,8 +8,24 @@ def flood_fill(array, start_pos, fill_value):
         array[row][col] = fill_value
 
 
-def flood_select(array, start_pos, selected=None):
-    """Return list of positions in order that they will be flood-filled"""
+def _valid_neighbors(array, position):
+    """Return neighbors of a position that are inside the array."""
+    row, col = position
+    num_rows, num_cols = array.shape
+    neighbors = []
+    for row_delta, col_delta in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
+        new_row, new_col = row + row_delta, col + col_delta
+        if new_row >= 0 and new_row < num_rows:
+            if new_col >= 0 and new_col < num_cols:
+                neighbors.append((new_row, new_col))
+    return neighbors
+
+
+def recursive_flood_select(array, start_pos, selected=None):
+    """Return list of positions in order that they will be flood-filled
+
+    This implementation is recursive so it will be extremely limited!
+    """
     if selected is None:
         selected = [[False for _ in array[0]] for _ in array]
 
@@ -26,22 +44,33 @@ def flood_select(array, start_pos, selected=None):
             # Does the neighbor have the same value?
             if array[row][col].all() == array[new_row][new_col].all():
                 new_pos = (new_row, new_col)
-                rest.extend(flood_select(array, new_pos, selected))
+                rest.extend(recursive_flood_select(array, new_pos, selected))
     return [start_pos] + rest
 
 
-def test_flood_select():
-    array = [[0, 0, 1, 0, 0],
-             [0, 1, 1, 1, 0],
-             [1, 1, 1, 1, 1],
-             [0, 1, 1, 1, 0],
-             [0, 0, 1, 0, 0]]
+def flood_select(array, start_pos):
+    """Return list of positions in order that they will be flood-filled
 
-    # start from top middle
-    expected = [(0, 2), (1, 2), (1, 3), (2, 3), (2, 4), (3, 3), (3, 2), (2, 2),
-                (2, 1), (1, 1), (3, 1), (2, 0), (4, 2)]
-    assert flood_select(array, (0, 2)) == expected
+    Stack-based implementation- keep track of which locations still need to be
+    processed on a stack.
+    """
+    stack = [start_pos]
+    target_color = array[start_pos]
+    selection = []
+    visited = numpy.zeros(array.shape, dtype=numpy.bool8)
 
-    # start from top right
-    expected = [(0, 4), (1, 4), (0, 3)]
-    assert flood_select(array, (0, 4)) == expected
+    # Pop from stack and push neighbors on until it's empty
+    while stack:
+        print "stack: {}".format(stack)
+        current_pos = stack.pop()
+        color_matches = (array[current_pos] == target_color).all()
+        if color_matches and not(visited[current_pos]):
+            selection.append(current_pos)
+            stack.extend(_valid_neighbors(array, current_pos))
+        visited[current_pos] = True
+    return selection
+
+
+def test_recursive_iterative_flood_select():
+    """Make sure recursive and iterative flood select have the same results"""
+    pass
